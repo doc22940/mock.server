e0d4edd0390fdc77a230c255f70249b844237b44
Simon Mollweide <simon.mollweide@namics.com>
2016-03-23 08:22:11 +0100

    RefactorServices: remove unused variables from mock-server.js

--- mock-server.js
+++ mock-server.js
@@ -1,63 +1,34 @@
 
 'use strict';
 
-var express = require('express'),
-	app = express(),
-	fs = require('fs'),
-	bodyParser = require('body-parser'),
-	Utils = require('./lib/Utils'),
-	_utils = new Utils(),
-	util = require('util'),
-	extend = util._extend,
-	ejs = require('ejs'),
-	Ui = require('./lib/ui'),
-	getResponseData = require('./lib/getResponseData'),
-	getFunc = require('./lib/getFunc'),
-	getPreferences = require('./lib/getPreferences'),
-	SwaggerImport = require('./lib/SwaggerImport'),
-	swaggerLog = require('./lib/SwaggerLog'),
-	DTOToClassConverter = require('./lib/DTOToClassConverter'),
-	GetResponse = require('./lib/GetResponse'),
-	ValidatorResponse = require('./lib/ValidatorResponse'),
-	ValidatorResponses = require('./lib/ValidatorResponses'),
-	validatorLog = require('./lib/ValidatorLog'),
-	findFolder = require('./lib/findFolder'),
-	AppControllerSingleton = require('./controller/AppController'),
-	UiController,
-	MockController,
-	SchemaController,
-	SwaggerImportController,
-	DTOController,
-	ResponseController,
-	PreferencesController,
-	ValidatorController;
+var AppControllerSingleton = require('./controller/AppController');
 
 module.exports = function (options) {
 
 	AppControllerSingleton.getInstance(options);
 
-	UiController = require('./controller/UiController');
+	var UiController = require('./controller/UiController');
 	new UiController();
 
-	SchemaController = require('./controller/SchemaController');
+	var SchemaController = require('./controller/SchemaController');
 	new SchemaController();
 
-	SwaggerImportController = require('./controller/SwaggerImportController');
+	var SwaggerImportController = require('./controller/SwaggerImportController');
 	new SwaggerImportController();
 
-	DTOController = require('./controller/DTOController');
+	var DTOController = require('./controller/DTOController');
 	new DTOController();
 
-	ResponseController = require('./controller/ResponseController');
+	var ResponseController = require('./controller/ResponseController');
 	new ResponseController();
 
-	PreferencesController = require('./controller/PreferencesController');
+	var PreferencesController = require('./controller/PreferencesController');
 	new PreferencesController();
 
-	ValidatorController = require('./controller/ValidatorController');
+	var ValidatorController = require('./controller/ValidatorController');
 	new ValidatorController();
 
-	MockController = require('./controller/MockController');
+	var MockController = require('./controller/MockController');
 	new MockController();
 
 };
