3964a37296d810c1fc4166d26c07b2608f694653
Simon Mollweide <simon.mollweide@namics.com>
2016-04-23 00:25:56 +0200

    [tests] #2: add test for response func - array

--- test/expected/dto-response-func/ResponseFuncTestDTO.js
+++ test/expected/dto-response-func/ResponseFuncTestDTO.js
@@ -1,10 +1,24 @@
 
 var faker = require('faker');
 
+function _getArray(getData) {
+	var out = [],
+		len = faker.random.number(10) + 1,
+		i;
+
+	for (i = 0; i < len; i += 1) {
+		out.push(getData());
+	}
+
+	return out;
+}
+
 module.exports = {
 
 	importedResponseFuncTestDTO: function () {
 		return JSON.stringify({
+  "list": _getArray(function () {return {  "country": JSON.parse(require('../func-imported/CountryWsDTO.js').importedCountryWsDTO()),  "email": faker.internet.email()};}),
+  "items": _getArray(function () {return JSON.parse(require('../func-imported/CountryWsDTO.js').importedCountryWsDTO());}),
   "image": JSON.parse(require('../func-imported/CountryWsDTO.js').importedCountryWsDTO()),
   "exampleString": faker.lorem.word(),
   "exampleNumber": faker.random.number(),
@@ -15,7 +29,7 @@ module.exports = {
   "country": JSON.parse(require('../func-imported/CountryWsDTO.js').importedCountryWsDTO()),
   "countryIsocode": "CH",
   "email": faker.internet.email(),
-  "phone": faker.phoneNumber.phoneNumber(),
+  "phone": faker.phone.phoneNumber(),
   "postalCode": faker.address.zipCode(),
   "streetName": faker.address.streetName(),
   "streetNumber": faker.random.number(),
