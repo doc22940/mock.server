03339d55c24735ab7cc775267ff4762bba66883c
Simon Mollweide <simon.mollweide@namics.com>
2016-04-25 07:13:01 +0200

    [tests] #2: add test for create faker file functionallity

--- test/tests-dto-response-func.js
+++ test/tests-dto-response-func.js
@@ -4,12 +4,20 @@ var assert = require('assert');
 module.exports = function(serverOptions, _getFile) {
 
 	var pathFunc = serverOptions.swaggerImport.responseFuncPath,
+		pathRest = serverOptions.swaggerImport.dest,
 		pathExpSwagger = './test/expected/dto-response-func';
 
-	// AddressResponseWsDTO
+	// ResponseFuncTestDTO
 	it('DTO Response Func (ResponseFuncTestDTO) should be written!', function () {
 		var data = _getFile(pathFunc + '/ResponseFuncTestDTO.js'),
 			expected = _getFile(pathExpSwagger + '/ResponseFuncTestDTO.js');
+		assert.equal(data, expected);
+	});
+
+	// success-faker
+	it('success-faker file should be written!', function () {
+		var data = _getFile(pathRest + '/{id}/#addresses/GET/mock/success-faker.json'),
+			expected = _getFile(pathExpSwagger + '/success-faker.json');
 		assert.equal(data, expected);
 	});
 
