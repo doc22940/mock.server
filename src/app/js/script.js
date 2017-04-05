62b337f56a7848dd017f13f8b0b3e620a4701e27
Simon Mollweide <simon.mollweide@namics.com>
2017-04-05 20:03:57 +0200

    [add] validation result alert box on homepage

--- src/app/js/script.js
+++ src/app/js/script.js
@@ -1,191 +1,212 @@
-$(document).ready(function() {
 
-	$('.page-header pre code, .top-resource-description pre code').each(function(i, block) {
-		hljs.highlightBlock(block);
-	});
+/* global window, document, hljs, JSONFormatter, jQuery */
 
-	$('[data-toggle]').click(function() {
-		var selector = $(this).data('target') + ' pre code';
+((($) => {
+	$(document).ready(function () {
 
-		$(selector).each(function(i, block) {
-			//console.dir(block);
+		$('.page-header pre code, .top-resource-description pre code').each(function (i, block) {
 			hljs.highlightBlock(block);
 		});
-	});
 
-	$('.js-format-json').each(function () {
-		var $elem = $(this),
-			jsonData = JSON.parse($('.js-format-json__json', $elem).html()),
-			formatter = new JSONFormatter(jsonData, 2);
+		$('[data-toggle]').click(function () {
+			var selector = $(this).data('target') + ' pre code';
 
-		$('.js-format-json__outlet-formatted', $elem).html(formatter.render());
-	});
-
-	$('[data-load-code-url]').click(function() {
-		var path = $(this).data('load-code-url'),
-			selector = $(this).attr('href') + ' pre code';
-
-		$.ajax({
-			url: '/service/mock-preview',
-			dataType: 'text',
-			data: {
-				path: path
-			},
-			cache: false,
-			success: function (data) {
-				$(selector).html(data).each(function(i, block) {
-					hljs.highlightBlock(block);
-				});
-			}
-		});
-	});
-
-	/**
-	 *
-	 * @method _storeSettings
-	 * @param {object} event
-	 * @private
-	 */
-	function _storeSettings (event) {
-		var $ipt = $(event.currentTarget),
-			$form = $ipt.closest('.js-form-change-save');
-
-		event.preventDefault();
-
-		$ipt.attr('data-status', 'saving');
-
-		$.ajax({
-			url: $form.attr('action'),
-			type: $form.attr('method'),
-			data: {
-				key: $ipt.attr('name'),
-				value: $ipt.val()
-			},
-			success: function () {
-				$ipt.attr('data-status', 'saved');
-			},
-			error: function () {
-				$ipt.attr('data-status', 'error');
-			}
-		});
-	}
-
-	$('.js-form-change-save').on('submit', _storeSettings);
-
-	$('.js-form-change-save input').
-		on('keydown', function (event) {
-			$(event.currentTarget).attr('data-status', 'not-saved');
-		}).
-		on('change', _storeSettings)
-	;
-
-	$('.js-change-response-data').on('change', function (event) {
-		var $radio = $(event.currentTarget);
-
-		$.ajax({
-			url: '/service/expected-response',
-			type: 'post',
-			data: {
-				value: $radio.val(),
-				path: $radio.data('path')
-			}
+			$(selector).each(function (i, block) {
+				hljs.highlightBlock(block);
+			});
 		});
 
-	});
+		$('.js-format-json').each(function () {
+			var $elem = $(this);
+			var jsonData = JSON.parse($('.js-format-json__json', $elem).html());
+			var formatter = new JSONFormatter(jsonData, 2);
 
-	function getLogRows (data) {
-		var type,
-			out;
+			$('.js-format-json__outlet-formatted', $elem).html(formatter.render());
+		});
 
-		if (data instanceof Array && data.length > 0) {
-			out = data.map(function (msg) {
+		$('[data-load-code-url]').click(function () {
+			var path = $(this).data('load-code-url');
+			var selector = $(this).attr('href') + ' pre code';
 
-				if (msg.type === 'error') {
-					type = 'danger';
-				} else if (msg.type === 'warn') {
-					type = 'warning';
-				} else if (msg.type === 'neutral') {
-					type = 'active';
-				} else {
-					type = msg.type;
-				}
+			$.ajax({
+				url: '/service/mock-preview',
+				dataType: 'text',
+				data: {
+					path: path,
+				},
+				cache: false,
+				success: function (data) {
+					$(selector).html(data).each(function (i, block) {
+						hljs.highlightBlock(block);
+					});
+				},
+			});
+		});
 
-				return '' +
-					'<tr>' +
-					'<td class="' + type + '" style="width:6px;padding:0;"></td>' +
-					'<td>' + msg.time + '</td>' +
-					'<td>' + msg.msg + '</td>' +
-					'</tr>';
+		/**
+		 * @param {Object} event - the click event object
+		 * @returns {void}
+		 * @private
+		 */
+		function _storeSettings(event) {
+			var $ipt = $(event.currentTarget);
+			var $form = $ipt.closest('.js-form-change-save');
+
+			event.preventDefault();
+
+			$ipt.attr('data-status', 'saving');
+
+			$.ajax({
+				url: $form.attr('action'),
+				type: $form.attr('method'),
+				data: {
+					key: $ipt.attr('name'),
+					value: $ipt.val(),
+				},
+				success: function () {
+					$ipt.attr('data-status', 'saved');
+				},
+				error: function () {
+					$ipt.attr('data-status', 'error');
+				},
 			});
 		}
 
-		return out;
-	}
+		$('.js-form-change-save').on('submit', _storeSettings);
 
-	$('.js-swagger-import-run').on('click', function (event) {
-		var $btn = $(event.currentTarget),
-			$mBody = $btn.closest('.modal-body'),
-			$outlet = $('.js-swagger-import-outlet', $mBody),
-			$result = $('.js-swagger-import-outlet-result', $mBody);
+		$('.js-form-change-save input').
+			on('keydown', function (event) {
+				$(event.currentTarget).attr('data-status', 'not-saved');
+			}).
+			on('change', _storeSettings)
+		;
 
-		$btn.remove();
-		$outlet.html('Loading ...');
+		$('.js-change-response-data').on('change', function (event) {
+			var $radio = $(event.currentTarget);
 
-		$.ajax({
-			url: '/service/swagger-import',
-			type: 'get',
-			success: function (data) {
-				$result.html(getLogRows(data));
-				$outlet.html('<a href="/" class="btn btn-lg btn-primary">Reload</a>');
-			}
-		});
-	});
+			$.ajax({
+				url: '/service/expected-response',
+				type: 'post',
+				data: {
+					value: $radio.val(),
+					path: $radio.data('path'),
+				},
+			});
 
-	$('.js-validate-single').on('click', function (event) {
-		event.preventDefault();
-
-		var $btn = $(event.currentTarget),
-			$modal = $('#_modal_validate_single'),
-			$outlet = $('.js-outlet', $modal),
-			$result = $('.js-outlet-result', $modal);
-
-		$modal.modal();
-		$outlet.html('Loading ...');
-
-		$.ajax({
-			url: '/service/validation/response',
-			type: 'get',
-			data: {
-				path: $btn.data('path'),
-				method: $btn.data('method'),
-				expected: $btn.data('expected')
-			},
-			success: function (data) {
-				$result.html(getLogRows(data));
-				$outlet.html('<a href="/" class="btn btn-lg btn-primary">Reload</a>');
-			}
 		});
 
-	});
+		/**
+		 * @param {Array} data - list of messages
+		 * @returns {Array} dom elements
+		 * @private
+		 */
+		function getLogRows(data) {
+			var type;
+			var out;
 
-	$('.js-validate').on('click', function (event) {
-		event.preventDefault();
+			if (data instanceof Array && data.length > 0) {
+				out = data.map(function (msg) {
 
-		var $modal = $('#_modal_validate'),
-			$outlet = $('.js-outlet', $modal),
-			$result = $('.js-outlet-result', $modal);
+					switch (msg.type) {
+						case 'error':
+							type = 'danger';
+							break;
+						case 'warn':
+							type = 'warning';
+							break;
+						case 'neutral':
+							type = 'active';
+							break;
+						default:
+							type = msg.type;
+							break;
+					}
 
-		$modal.modal();
-		$outlet.html('Loading ...');
-
-		$.ajax({
-			url: '/service/validation/responses',
-			type: 'get',
-			success: function (data) {
-				$result.html(getLogRows(data));
-				$outlet.html('<a href="/" class="btn btn-lg btn-primary">Reload</a>');
+					return '' +
+						'<tr>' +
+						'<td class="' + type + '" style="width:6px;padding:0;"></td>' +
+						'<td>' + msg.time + '</td>' +
+						'<td>' + msg.msg + '</td>' +
+						'</tr>';
+				});
 			}
+
+			return out;
+		}
+
+		$('.js-swagger-import-run').on('click', function (event) {
+			var $btn = $(event.currentTarget);
+			var $mBody = $btn.closest('.modal-body');
+			var $outlet = $('.js-swagger-import-outlet', $mBody);
+			var $result = $('.js-swagger-import-outlet-result', $mBody);
+
+			$btn.remove();
+			$outlet.html('Loading ...');
+
+			$.ajax({
+				url: '/service/swagger-import',
+				type: 'get',
+				success: function (data) {
+					$result.html(getLogRows(data));
+					$outlet.html('<a href="/" class="btn btn-lg btn-primary">Reload</a>');
+				},
+			});
 		});
 
+		$('.js-open-validation-result').on('click', function (event) {
+			var $btn = $(event.currentTarget);
+			var $modal = $($btn.data('target'));
+			var $result = $('.js-outlet-result', $modal);
+
+			$result.html(getLogRows(window.validationResult));
+		});
+
+		$('.js-validate-single').on('click', function (event) {
+			event.preventDefault();
+
+			var $btn = $(event.currentTarget);
+			var $modal = $('#_modal_validate_single');
+			var $outlet = $('.js-outlet', $modal);
+			var $result = $('.js-outlet-result', $modal);
+
+			$modal.modal();
+			$outlet.html('Loading ...');
+
+			$.ajax({
+				url: '/service/validation/response',
+				type: 'get',
+				data: {
+					path: $btn.data('path'),
+					method: $btn.data('method'),
+					expected: $btn.data('expected'),
+				},
+				success: function (data) {
+					$result.html(getLogRows(data));
+					$outlet.html('<a href="/" class="btn btn-lg btn-primary">Reload</a>');
+				},
+			});
+
+		});
+
+		$('.js-validate').on('click', function (event) {
+			event.preventDefault();
+
+			var $modal = $('#_modal_validate');
+			var $outlet = $('.js-outlet', $modal);
+			var $result = $('.js-outlet-result', $modal);
+
+			$modal.modal();
+			$outlet.html('Loading ...');
+
+			$.ajax({
+				url: '/service/validation/responses',
+				type: 'get',
+				success: function (data) {
+					$result.html(getLogRows(data));
+					$outlet.html('<a href="/" class="btn btn-lg btn-primary">Reload</a>');
+				},
+			});
+
+		});
 	});
-});
+})(jQuery));
