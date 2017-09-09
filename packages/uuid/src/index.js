c484e9c32d078ea7fc46768832b0f9f44b0c3aed
Simon Mollweide <simon.mollweide@namics.com>
2017-09-09 12:28:04 +0200

    [change] update uuid package

--- packages/uuid/src/index.js
+++ packages/uuid/src/index.js
@@ -4,6 +4,6 @@ export function decode(encodedPath: string): string {
 	return new Buffer(encodedPath, "base64").toString("ascii").replace(/\/\//g, "/");
 }
 
-export function encode(path: string) {
+export function encode(path: string): string {
 	return new Buffer(path).toString("base64");
 }
--- packages/uuid/src/index.spec.js
+++ packages/uuid/src/index.spec.js
@@ -1,3 +1,4 @@
+/* global describe it expect */
 import { encode } from "./index.js";
 
 describe("encode", () => {
