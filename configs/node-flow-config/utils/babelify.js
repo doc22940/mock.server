c4f8ecf598bee47aa1253c55b3f2d95f8b75e2c1
Simon Mollweide <simon.mollweide@namics.com>
2017-10-14 12:46:38 +0200

    [setup] babelify write executable

--- configs/node-flow-config/utils/babelify.js
+++ configs/node-flow-config/utils/babelify.js
@@ -1,18 +1,18 @@
 /* eslint global-require: 0*/
 /* eslint no-console: 0*/
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const path = require("path");
-const makeDir = require("make-dir");
-const babel = require("babel-core");
-const print = require("./print");
-const paths = require("../config/paths");
-const getBuildPackage = require("./get-build-package");
-const babelTransformOptions = require("../config/babel-transform-options");
+const fs = require('fs');
+const path = require('path');
+const makeDir = require('make-dir');
+const babel = require('babel-core');
+const print = require('./print');
+const paths = require('../config/paths');
+const getBuildPackage = require('./get-build-package');
+const babelTransformOptions = require('../config/babel-transform-options');
 
-process.env.BABEL_ENV = "production";
-process.env.NODE_ENV = "production";
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
 
 function babelify(file) {
 	try {
@@ -23,9 +23,11 @@ function babelify(file) {
 		const result = babel.transformFileSync(file, babelTransformOptions);
 		const target = file.replace(packageBuildData.src, packageBuildData.dest);
 		const fileName = path.basename(file);
-		makeDir.sync(target.replace(fileName, ""));
-		fs.writeFileSync(target, result.code);
-		print.log(`wrote file ${target.replace(paths.currentDirectory, "")}`);
+		makeDir.sync(target.replace(fileName, ''));
+		fs.writeFileSync(target, result.code, {
+			mode: 0o755,
+		});
+		print.log(`wrote file ${target.replace(paths.currentDirectory, '')}`);
 	} catch (err) {
 		console.error(err);
 	}
